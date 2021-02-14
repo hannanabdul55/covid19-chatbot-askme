f11bb5d9495742c0d640c31ce9876d5b9c456400
Abdul Hannan Kanji <hannanabdul55@gmail.com>
2021-02-13 20:31:50 -0500

    reformal

--- main.py
+++ main.py
@@ -5,67 +5,78 @@ import sys
 
 from datetime import datetime, date
 from dateutil import parser
+
 app = Flask(__name__)
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+
 sample_response = {
-  "fulfillmentMessages": [
-    {
-      "text": {
-        "text": [
-          "Sample response from the  covid webhook"
-        ]
-      }
-    }
-  ]
+    "fulfillmentMessages": [
+        {
+            "text": {
+                "text": [
+                    "Sample response from the  covid webhook"
+                ]
+            }
+        }
+    ]
 }
 
 error_response = {
-  "fulfillmentMessages": [
-    {
-      "text": {
-        "text": [
-          "Something unexpected happened. Please try again"
-        ]
-      }
-    }
-  ]
+    "fulfillmentMessages": [
+        {
+            "text": {
+                "text": [
+                    "Something unexpected happened. Please try again"
+                ]
+            }
+        }
+    ]
 }
+
 
 def create_response_obj(resp_str):
     res = sample_response
     res['fulfillmentMessages'][0]['text']['text'] = [resp_str]
     return res
 
+
 unexpected_error_response = {
-  "fulfillmentMessages": [
-    {
-      "text": {
-        "text": [
-          "Error encountered. Please try again"
-        ]
-      }
-    }
-  ]
+    "fulfillmentMessages": [
+        {
+            "text": {
+                "text": [
+                    "Error encountered. Please try again"
+                ]
+            }
+        }
+    ]
 }
 
-locations = pd.read_csv('https://github.com/reichlab/covid19-forecast-hub/raw/master/data-locations/locations.csv')
-deaths = pd.read_csv('https://github.com/hannanabdul55/covid19-forecast-hub/raw/master/data-truth/truth-Incident%20Deaths.csv').merge(locations, how='left', on=['location', 'location_name']).dropna(subset=['abbreviation'])
+locations = pd.read_csv(
+    'https://github.com/reichlab/covid19-forecast-hub/raw/master/data-locations/locations.csv')
+deaths = pd.read_csv(
+    'https://github.com/hannanabdul55/covid19-forecast-hub/raw/master/data-truth/truth-Incident%20Deaths.csv').merge(
+    locations, how='left', on=['location', 'location_name']).dropna(subset=['abbreviation'])
 
-cases = pd.read_csv('https://github.com/hannanabdul55/covid19-forecast-hub/raw/master/data-truth/truth-Incident%20Cases.csv').merge(locations, how='left', on=['location', 'location_name']).dropna(subset=['abbreviation'])
+cases = pd.read_csv(
+    'https://github.com/hannanabdul55/covid19-forecast-hub/raw/master/data-truth/truth-Incident%20Cases.csv').merge(
+    locations, how='left', on=['location', 'location_name']).dropna(subset=['abbreviation'])
 deaths['date'] = pd.to_datetime(deaths.date, infer_datetime_format=True)
 cases['date'] = pd.to_datetime(cases.date, infer_datetime_format=True)
 
 date_format = '%Y-%m-%d'
 text_date_format = '%a %b %d %Y'
 
+
 # deaths = deaths.merge(locations, how='left', on=['location', 'location_name'])
 # cases = cases.merge(locations, how='left', on=['location', 'location_name'])
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
         data = json.loads(request.data)
@@ -83,7 +94,8 @@ def parse_response(req):
         dates = req['queryResult']['parameters']['date-period']
         date = None
         range = False
-        if 'date-time' in req['queryResult']['parameters'] and req['queryResult']['parameters']['date-time']!='':
+        if 'date-time' in req['queryResult']['parameters'] and req['queryResult']['parameters'][
+            'date-time'] != '':
             date = parser.parse(req['queryResult']['parameters']['date-time'])
         elif 'endDate' in dates and 'startDate' in dates:
             range = True
@@ -100,20 +112,24 @@ def parse_response(req):
         val = -1
         if intent['displayName'] == "Deaths":
             if not range:
-                val = deaths.loc[(deaths['location_name'] == state) & (deaths['date'] == date.strftime(date_format))]['value'].sum()
+                val = deaths.loc[(deaths['location_name'] == state) & (
+                            deaths['date'] == date.strftime(date_format))]['value'].sum()
             else:
                 val = deaths.loc[(deaths['location_name'] == state) & (
-                            deaths['date'] >= start_date.strftime(date_format))& (
-                            deaths['date'] <= end_date.strftime(date_format))]['value'].sum()
+                        deaths['date'] >= start_date.strftime(date_format)) & (
+                                         deaths['date'] <= end_date.strftime(date_format))][
+                    'value'].sum()
         if intent['displayName'] == "Cases":
             if not range:
-                val = cases.loc[(cases['location_name'] == state) & (cases['date'] == date.strftime('%Y-%m-%d'))]['value'].sum()
+                val = cases.loc[(cases['location_name'] == state) & (
+                            cases['date'] == date.strftime('%Y-%m-%d'))]['value'].sum()
             else:
                 val = cases.loc[(cases['location_name'] == state) & (
-                            cases['date'] >= start_date.strftime(date_format))& (
-                            cases['date'] <= end_date.strftime(date_format))]['value'].sum()
+                        cases['date'] >= start_date.strftime(date_format)) & (
+                                        cases['date'] <= end_date.strftime(date_format))][
+                    'value'].sum()
             # val = cases.loc[(cases['location_name'] == state) & (cases['date'] == date.strftime('%Y-%m-%d'))]['value'].sum()
-        if val ==0:
+        if val == 0:
             final_resp = f"There were no {intent['displayName'].lower()} "
         else:
             final_resp = f"There were {val} {intent['displayName'].lower()} "
@@ -121,10 +137,8 @@ def parse_response(req):
             final_resp += f"from {start_date.strftime(text_date_format)} to {end_date.strftime(text_date_format)} "
         elif date is not None:
             final_resp += f"on {date.strftime(text_date_format)} "
-        final_resp += f"for the state of {state}."
+        final_resp += f"in the state of {state}."
         return create_response_obj(final_resp)
-
-
 
     except KeyError as e:
         eprint("KeyError parsing response", e)
@@ -132,7 +146,6 @@ def parse_response(req):
     # except:
     #     eprint("Unexpected error parsing response")
     #     return unexpected_error_response
-
 
 
 if __name__ == '__main__':
